6df1a0a9fcc740e7a7c5212c7c4f5b33ea8e7379
arjay-abrenica <arjayabrenica30@gmail.com>
2026-07-08 15:32:24 +0800

    feat: update open-next configuration to include build command and external dependencies

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,3 +1,32 @@
+// const config = {
+//   default: {
+//     override: {
+//       wrapper: "cloudflare-node",
+//       converter: "edge",
+//       proxyExternalRequest: "fetch",
+//       incrementalCache: "dummy",
+//       tagCache: "dummy",
+//       queue: "dummy",
+//     },
+//   },
+//   edgeExternals: ["node:crypto"],
+//   middleware: {
+//     external: true,
+//     override: {
+//       wrapper: "cloudflare-edge",
+//       converter: "edge",
+//       proxyExternalRequest: "fetch",
+//       incrementalCache: "dummy",
+//       tagCache: "dummy",
+//       queue: "dummy",
+//     },
+//   },
+// };
+
+// export default config;
+
+import { defineHexConfig } from '@opennextjs/cloudflare';
+
 const config = {
   default: {
     override: {
@@ -8,6 +37,8 @@ const config = {
       tagCache: "dummy",
       queue: "dummy",
     },
+    buildCommand: 'npx @opennextjs/cloudflare',
+    external: ['pg-cloudflare'], 
   },
   edgeExternals: ["node:crypto"],
   middleware: {
