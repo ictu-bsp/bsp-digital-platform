6441374232d975aa7915eccf7be6079170d9db0b
arjay-abrenica <arjayabrenica30@gmail.com>
2026-07-08 15:54:46 +0800

    feat: update open-next configuration to include build command and external dependencies

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,30 +1,30 @@
-const config = {
-  default: {
-    override: {
-      wrapper: "cloudflare-node",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
-    },
-    external: ['pg-cloudflare'],
-  },
-  edgeExternals: ["node:crypto"],
-  middleware: {
-    external: true,
-    override: {
-      wrapper: "cloudflare-edge",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
-    },
-  },
-};
+// const config = {
+//   default: {
+//     override: {
+//       wrapper: "cloudflare-node",
+//       converter: "edge",
+//       proxyExternalRequest: "fetch",
+//       incrementalCache: "dummy",
+//       tagCache: "dummy",
+//       queue: "dummy",
+//     },
+//     external: ['pg-cloudflare'],
+//   },
+//   edgeExternals: ["node:crypto"],
+//   middleware: {
+//     external: true,
+//     override: {
+//       wrapper: "cloudflare-edge",
+//       converter: "edge",
+//       proxyExternalRequest: "fetch",
+//       incrementalCache: "dummy",
+//       tagCache: "dummy",
+//       queue: "dummy",
+//     },
+//   },
+// };
 
-export default config;
+// export default config;
 
 // const config = {
 //   default: {
@@ -54,3 +54,32 @@ export default config;
 // };
 
 // export default config;
+
+const config = {
+  default: {
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+    buildCommand: 'npx @opennextjs/cloudflare',
+  },
+  
+  edgeExternals: ["node:crypto", "pg-cloudflare"],
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
+};
+
+export default config;
