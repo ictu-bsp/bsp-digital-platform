d1ab9924a1d1b5ff8ee9efb0dc1a2f21d9d947e7
De-Las-Alas <reuben.jonn.de.las.alas@adamson.edu.ph>
2026-07-14 07:49:27 +0800

    Attempt to fix the Main merging issue

--- src/app/scout/profile/page.tsx
+++ src/app/scout/profile/page.tsx
@@ -1,7 +1,7 @@
 //src/app/scout/profile/page.tsx
 
 import { redirect } from "next/navigation";
-import ProfileClient from "./ProfileClient";
+import ProfileClient from "./ClientProfile";
 import { getCurrentUser } from "@/lib/auth/current-user";
 
 export default async function ProfilePage() {
