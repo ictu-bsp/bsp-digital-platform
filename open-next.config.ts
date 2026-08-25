4578e00c320ad96399c3a7b0391b90653b3c4ba4
arjayAbrenica <arjayabrenica30@gmail.com>
2026-08-25 10:06:22 +0800

    Comment out config object in open-next.config.ts
    Commented out the entire configuration object in open-next.config.ts.

--- open-next.config.ts
+++ open-next.config.ts
@@ -55,31 +55,31 @@
 
 // export default config;
 
-const config = {
-  default: {
-    override: {
-      wrapper: "cloudflare-node",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
-    },
-    buildCommand: 'npx @opennextjs/cloudflare',
-  },
-  
-  edgeExternals: ["node:crypto", "pg-cloudflare"],
-  middleware: {
-    external: true,
-    override: {
-      wrapper: "cloudflare-edge",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-      incrementalCache: "dummy",
-      tagCache: "dummy",
-      queue: "dummy",
-    },
-  },
-};
-
-export default config;
+// const config = {
+//  default: {
+//   override: {
+//      wrapper: "cloudflare-node",
+//      converter: "edge",
+//      proxyExternalRequest: "fetch",
+//      incrementalCache: "dummy",
+//      tagCache: "dummy",
+//      queue: "dummy",
+//    },
+//    buildCommand: 'npx @opennextjs/cloudflare',
+//  },
+//  
+//  edgeExternals: ["node:crypto", "pg-cloudflare"],
+//  middleware: {
+//    external: true,
+//    override: {
+//      wrapper: "cloudflare-edge",
+//      converter: "edge",
+//      proxyExternalRequest: "fetch",
+//      incrementalCache: "dummy",
+//      tagCache: "dummy",
+//      queue: "dummy",
+//    },
+//  },
+// };
+//
+// export default config;
